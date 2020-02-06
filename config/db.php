150f10bcfc24098c1af23cd52145587546f7753a
peter <peter@192.168.1.65>
2020-02-06 23:27:05 +0100

    local db connection

--- config/db.php
+++ config/db.php
@@ -1,15 +1,33 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host='.((strpos($_SERVER['SERVER_NAME'],'printline.sk'))? 'localhost;unix_socket=/tmp/mariadb55.sock':'mariadb55.websupport.sk:3310').';dbname=zero.master',
-    //'username' => 'root',
-    //'password' => '',
-    'dsn' => 'mysql:host=localhost;dbname=zero.master',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-    'enableSchemaCache' => true,
-    'schemaCacheDuration' => 600,
-    'schemaCache' => 'cache',
-];
+if (strpos($_SERVER['SERVER_NAME'], 'printline.sk')) {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=' . ((strpos($_SERVER['SERVER_NAME'], 'printline.sk')) ? 'localhost;unix_socket=/tmp/mariadb55.sock' : 'mariadb55.websupport.sk:3310') . ';dbname=ccwz0d43',
+        'username' => 'ccwz0d43',
+        'password' => '7kF4JMFguc',
+//    'dsn' => 'mysql:host=localhost;dbname=ccwz0d43',
+//    'username' => 'root',
+//    'password' => '',
+        'charset' => 'utf8',
+        'enableSchemaCache' => true,
+        'schemaCacheDuration' => 600,
+        'schemaCache' => 'cache',
+    ];
+} else {
+    return [
+        'class' => 'yii\db\Connection',
+        // 'dsn' => 'mysql:host=' . ((strpos($_SERVER['SERVER_NAME'], 'printline.sk')) ? 'localhost;unix_socket=/tmp/mariadb55.sock' : 'mariadb55.websupport.sk:3310') . ';dbname=ccwz0d43',
+        //'username' => 'ccwz0d43',
+        //'password' => '7kF4JMFguc',
+        'dsn' => 'mysql:host=localhost;dbname=zero.master',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'enableSchemaCache' => true,
+        'schemaCacheDuration' => 600,
+        'schemaCache' => 'cache',
+    ];
+}
+
+
